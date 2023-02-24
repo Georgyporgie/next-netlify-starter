8a4cd90139f1155bda5198ea746561a80d75bc7e
joris <97834949+Georgyporgie@users.noreply.github.com>
2023-02-24 16:08:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Take a Seat <code>and enjoy!</code>
         </p>
       </main>
 
